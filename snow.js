f0d0a58bc4d837092eb7bb40342cdf2b788bbc71
Libor Paciorek <lpaciorek@gmail.com>
2024-12-06 16:22:41 +0100

    Create snow after DOM is loaded - fix

--- snow.js
+++ snow.js
@@ -9,15 +9,15 @@ const snowDisabledLight = document.getElementById("color-scheme-light");
 const snowDisabledDark = document.getElementById("color-scheme-dark");
 
 const random = (num) => {
-  return Math.floor(Math.random() * num);
+    return Math.floor(Math.random() * num);
 };
 
 const getRandomStyles = () => {
-  const top = random(100);
-  const left = random(100);
-  const dur = random(10) + 10;
-  const size = random(25) + 25;
-  return `
+    const top = random(100);
+    const left = random(100);
+    const dur = random(10) + 10;
+    const size = random(25) + 25;
+    return `
     top: -${top}%;
     left: ${left}%;
     font-size: ${size}px;
@@ -26,43 +26,42 @@ const getRandomStyles = () => {
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-  const createSnow = (num) => {
-    document.getElementById("snowMain").appendChild(snowContainer);
-    for (let i = num; i > 0; i--) {
-      let snow = document.createElement("div");
-      snow.className = "snow";
-      snow.style.cssText = getRandomStyles();
-      snow.innerHTML = snowContent[random(3)];
-      snowContainer.append(snow);
-      snowContainer.style.opacity = "1";
+    const createSnow = (num) => {
+        document.getElementById("snowMain").appendChild(snowContainer);
+        for (let i = num; i > 0; i--) {
+            let snow = document.createElement("div");
+            snow.className = "snow";
+            snow.style.cssText = getRandomStyles();
+            snow.innerHTML = snowContent[random(3)];
+            snowContainer.append(snow);
+            snowContainer.style.opacity = "1";
+        }
     }
-  }
 
 
-const removeSnow = () => {
-  snowContainer.style.opacity = "0";
-  setTimeout(() => {
-    snowContainer.remove()
-  }, 500)
-}
+    const removeSnow = () => {
+        snowContainer.style.opacity = "0";
+        setTimeout(() => {
+            snowContainer.remove()
+        }, 500)
+    }
 
-window.addEventListener("load", () => {
-  createSnow(30);
-  setTimeout(removeSnow, 1000 * 60);
-});
+    window.addEventListener("load", () => {
+        createSnow(30);
+        setTimeout(removeSnow, 1000 * 60);
+    });
 
-field.addEventListener("change", myFunction);
+    field.addEventListener("change", myFunction);
 
-function myFunction() {
-  if (snowEnable.checked) {
-    createSnow(30);
-  } else if (snowDisabledDark.checked) {
-    removeSnow();
-  } else if (snowDisabledLight.checked) {
-    removeSnow();
-  } else {
-    removeSnow();
-  }
-}
-
+    function myFunction() {
+        if (snowEnable.checked) {
+            createSnow(30);
+        } else if (snowDisabledDark.checked) {
+            removeSnow();
+        } else if (snowDisabledLight.checked) {
+            removeSnow();
+        } else {
+            removeSnow();
+        }
+    }
 });
