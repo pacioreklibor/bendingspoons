ec3b40f28e0275f47514896414ae2bec0c7acb6a
Libor Paciorek <lpaciorek@gmail.com>
2024-12-06 16:00:05 +0100

    Create a new Snow (div element) instead change opacity. Removed debugging points.

--- snow.js
+++ snow.js
@@ -1,10 +1,12 @@
-const snowContainer = document.getElementById("snow-container");
+let snowContainer = document.createElement("div")
+snowContainer.id = "snow-container";
+
 const snowContent = ["&#10052", "&#10053", "&#10054"];
 
-var field = document.getElementById("checkbox");
-var snowEnable = document.getElementById("color-scheme-auto");
-var snowDisabledLight = document.getElementById("color-scheme-light");
-var snowDisabledDark = document.getElementById("color-scheme-dark");
+const field = document.getElementById("checkbox");
+const snowEnable = document.getElementById("color-scheme-auto");
+const snowDisabledLight = document.getElementById("color-scheme-light");
+const snowDisabledDark = document.getElementById("color-scheme-dark");
 
 const random = (num) => {
   return Math.floor(Math.random() * num);
@@ -24,46 +26,39 @@ const getRandomStyles = () => {
 };
 
 const createSnow = (num) => {
-  for (var i = num; i > 0; i--) {
-    var snow = document.createElement("div");
+  document.getElementById("snowMain").appendChild(snowContainer);
+  for (let i = num; i > 0; i--) {
+    let snow = document.createElement("div");
     snow.className = "snow";
     snow.style.cssText = getRandomStyles();
     snow.innerHTML = snowContent[random(3)];
     snowContainer.append(snow);
+    snowContainer.style.opacity = "1";
   }
 };
 
 const removeSnow = () => {
   snowContainer.style.opacity = "0";
-};
-
-const enableSnow = () => {
-  snowContainer.style.opacity = "1";
-};
+  setTimeout(() => {
+    snowContainer.remove()
+  }, 500)
+}
 
 window.addEventListener("load", () => {
   createSnow(30);
   setTimeout(removeSnow, 1000 * 60);
 });
 
-/*indow.addEventListener("click", () => {
-  removeSnow();
-});*/
-
 field.addEventListener("change", myFunction);
 
 function myFunction() {
   if (snowEnable.checked) {
-    console.log("snowEnabled ckecked");
-    enableSnow(30);
+    createSnow(30);
   } else if (snowDisabledDark.checked) {
-    console.log("snowDisabledDark checked");
     removeSnow();
   } else if (snowDisabledLight.checked) {
-    console.log("snowDisabledLight checked");
     removeSnow();
   } else {
-    console.log("Nothing is checked");
     removeSnow();
   }
 }
