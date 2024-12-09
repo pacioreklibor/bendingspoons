b5d6abc1270566ec8232531f904c54707e36a406
Libor Paciorek <lpaciorek@gmail.com>
2024-12-09 22:21:04 +0100

    Changed order of code - took createSnow, removeSnow and myFunction outside of DOMContentLoaded

--- snow.js
+++ snow.js
@@ -1,13 +1,22 @@
-let snowContainer = document.createElement("div")
+// Creating snowContainer constant including a new div element
+// and assign an ID to it.
+// The snowContainer represents each snowflake that will be generated.
+const snowContainer = document.createElement("div")
 snowContainer.id = "snow-container";
 
+// snowContent is an array of different snowflake styles.
 const snowContent = ["&#10052", "&#10053", "&#10054"];
 
-const field = document.getElementById("checkbox");
-const snowEnable = document.getElementById("color-scheme-auto");
+
+// Defining block of radio buttons to react on changes.
+const themeRadio = document.getElementById("theme-radio");
+
+// Defining each radio selection to know which one is selected.
+const snowEnabled = document.getElementById("color-scheme-snow");
 const snowDisabledLight = document.getElementById("color-scheme-light");
 const snowDisabledDark = document.getElementById("color-scheme-dark");
 
+// Random number generator
 const random = (num) => {
     return Math.floor(Math.random() * num);
 };
@@ -25,43 +34,43 @@ const getRandomStyles = () => {
   `;
 };
 
+const createSnow = (num) => {
+    document.getElementById("snowMain").appendChild(snowContainer);
+    for (let i = num; i > 0; i--) {
+        let snow = document.createElement("div");
+        snow.className = "snow";
+        snow.style.cssText = getRandomStyles();
+        snow.innerHTML = snowContent[random(3)];
+        snowContainer.append(snow);
+        snowContainer.style.opacity = "1";
+    }
+}
+
+function myFunction() {
+    if (snowEnabled.checked) {
+        createSnow(30);
+    } else if (snowDisabledDark.checked) {
+        removeSnow();
+    } else if (snowDisabledLight.checked) {
+        removeSnow();
+    } else {
+        removeSnow();
+    }
+}
+
+
+const removeSnow = () => {
+    snowContainer.style.opacity = "0";
+    setTimeout(() => {
+        snowContainer.remove()
+    }, 500)
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-    const createSnow = (num) => {
-        document.getElementById("snowMain").appendChild(snowContainer);
-        for (let i = num; i > 0; i--) {
-            let snow = document.createElement("div");
-            snow.className = "snow";
-            snow.style.cssText = getRandomStyles();
-            snow.innerHTML = snowContent[random(3)];
-            snowContainer.append(snow);
-            snowContainer.style.opacity = "1";
-        }
-    }
-
-
-    const removeSnow = () => {
-        snowContainer.style.opacity = "0";
-        setTimeout(() => {
-            snowContainer.remove()
-        }, 500)
-    }
-
     window.addEventListener("load", () => {
         createSnow(30);
         setTimeout(removeSnow, 1000 * 60);
     });
 
-    field.addEventListener("change", myFunction);
-
-    function myFunction() {
-        if (snowEnable.checked) {
-            createSnow(30);
-        } else if (snowDisabledDark.checked) {
-            removeSnow();
-        } else if (snowDisabledLight.checked) {
-            removeSnow();
-        } else {
-            removeSnow();
-        }
-    }
+    themeRadio.addEventListener("change", myFunction);
 });
